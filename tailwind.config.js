673121c4afaa4106bb61443030b18a6f6b87b972
hongsi0 <mushbaur@gmail.com>
2024-11-21 12:38:27 +0900

    [Chore] Add custom fonts and colors to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,28 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}" //수정
   ],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        transparent: "transparent",
+        white: "#FFFFFF",
+        black: "#000000",
+        primary: {
+          DEFAULT: "#FF8E8E",
+          100: "#FFF3F3", // subcolor 3
+          200: "#FFDCDC", // subcolor 2
+          300: "#FFAEAE", // subcolor 1
+          400: "#FF8E8E" // default 색상과 동일
+        },
+        gray: {
+          100: "#FAFAFA", // button, background
+          200: "#F5F5F5", // badge background
+          300: "#AAAAAA" // dark button
+        }
+      },
+      fontFamily: {
+        sans: ["Pretendard", "Inter", "system-ui", "Avenir", "Helvetica", "Arial", "sans-serif"]
+      }
+    }
   },
   plugins: []
 };

7a1589e4cb2e8abaf35462f641bf79d8fcb7d4c0
Siyoung <hongsizero@gmail.com>
2024-12-07 14:00:01 +0900

    feat: 견적서 화면 추가
    고객 견적서 화면 - 1:1 견적 요청
    미용사 견적서 화면 - 전체
    
    Related to #25, #29

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,6 @@ function App() {
           {/* customer/ -layout */}
           <Route path="/customer" element={<CustomerLayout />}>
             <Route path="home" element={<CustomerHome />} />
-            <Route path="quote" element={<CustomerQuote />} />
             <Route path="chat" element={<CustomerChat />} />
           </Route>
 
@@ -68,8 +67,8 @@ function App() {
               <Route index element={<CustomerSearchPage />} />
               <Route path=":shopId" element={<ShopDetailPage />} />
             </Route>
-            <Route path="shop/quote/:shopId" element={<ShopQuoteRequestPage />} />
-            <Route path="quote/request" element={<TotalQuoteRequestPage />} />
+            <Route path="shop/quotes/:shopId" element={<ShopQuoteRequestPage />} />
+            <Route path="quotes/request" element={<TotalQuoteRequestPage />} />
             <Route path="mypage" element={<CustomerMypage />} />
             <Route path="info" element={<UserInfo />} />
             <Route path="bookmarkedStore" element={<BookmarkedStore />} />
@@ -84,7 +83,6 @@ function App() {
           {/* groomer/ -layout */}
           <Route path="/groomer" element={<GroomerLayout />}>
             <Route path="home" element={<GroomerHome />} />
-            <Route path="quote" element={<GroomerQuote />} />
             <Route path="store" element={<GroomerStore />} />
             <Route path="chat" element={<GroomerChat />} />
           </Route>
--- src/pages/Quote/Customer/ShopQuoteRequestPage.jsx
+++ src/pages/Quote/Customer/ShopQuoteRequestPage.jsx
@@ -14,3 +14,5 @@ function ShopQuoteRequestPage(props) {
     </div>
   );
 }
+
+export default ShopQuoteRequestPage;

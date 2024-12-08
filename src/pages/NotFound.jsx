d08d8b363c76b7dfd1bc9d25d577afc4d6bf45eb
Suh-code <yunkisuh@naver.com>
2024-12-08 16:53:55 +0900

    Design : 404 Error 페이지 수정

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,15 @@
+import Lottie from "lottie-react";
+import NotFoundAnimation from "./NotFoundAnimation.json";
+import SubHeader from "@/components/common/SubHeader";
+
 const NotFound = () => {
-  return <div>404 - Page Not Found</div>;
+  return (
+    <div className="my-6 mt-60 flex flex-col items-center">
+      <SubHeader title="404 ERROR" />
+      <Lottie animationData={NotFoundAnimation} loop style={{ height: "300px", width: "300px" }} />
+      <p className="mt-30 text-center text-xl font-bold">잘못된 주소입니다.</p>
+    </div>
+  );
 };
 
 export default NotFound;

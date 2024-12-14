f9e0ecfe8723aade12a9972e0cb8f26b11ebb6af
seung-heee <tmdgmldi2595@naver.com>
2024-12-14 17:31:39 +0900

    Feat: customer banner swiper
    고객 메인화면 배너 swiper autoplay

--- src/components/Main/BannerSwiper.jsx
+++ src/components/Main/BannerSwiper.jsx
@@ -1,14 +1,48 @@
-import React from "react";
 import banner01 from "/Banner/banner1.jpg";
 import banner02 from "/Banner/banner2.jpg";
 import banner03 from "/Banner/banner3.jpg";
 import banner04 from "/Banner/banner4.jpg";
 import banner05 from "/Banner/banner5.jpg";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Autoplay, Scrollbar, A11y } from "swiper/modules";
+
 const BannerSwiper = () => {
   return (
-    <div className="p-2">
-      <img src={banner03} alt="" className="rounded-xl" />
+    <div className="mx-auto p-2">
+      <Swiper
+        modules={[Autoplay, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false
+        }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>
+          <img src={banner01} alt="Banner 01" className="h-[250px] w-[350px] rounded-xl object-fill" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner02} alt="Banner 02" className="h-[250px] w-[350px] rounded-xl object-fill" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner03} alt="Banner 03" className="h-[250px] w-[350px] rounded-xl object-fill" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner04} alt="Banner 04" className="h-[250px] w-[350px] rounded-xl object-fill" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={banner05} alt="Banner 04" className="h-[250px] w-[350px] rounded-xl object-fill" />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };

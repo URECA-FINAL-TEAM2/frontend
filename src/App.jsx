6e583d81c0f3cf23025cf85f82f570985598f495
seung-heee <tmdgmldi2595@naver.com>
2024-12-14 22:14:54 +0900

    Feat: PrivateRoutes 권한기반라우팅
    로그인하지 않은 상태에서 고객/미용사 페이지 강제 접근시 온보딩화면으로 라우팅
    고객 권한을 가진 사용자가 미용사 관련페이지 접근시 온보딩화면으로 라우팅
    미용사 권한을 가진 사용자가 고객 관련페이지 접근시 온보딩화면으로 라우팅

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,33 @@ import useAuthStore from "./store/authStore";
 import CustomerRoutes from "./Routes/CustomerRoutes";
 import GroomerRoutes from "./Routes/GroomerRoutes";
 import NotFound from "./pages/NotFound";
-import EditPortfolio from "./components/Mypage/Store/EditPortfolio";
-import PaymentCancel from "./pages/Payment/PaymentCancel";
-import PostReview from "./pages/Mypage/Reviews/PostReview";
-import CustomerQuote from "./pages/Customer/CustomerQuote";
-import CustomerQuoteDetailPage from "./pages/Quote/Customer/CustomerQuoteDetailPage";
-import TotalQuoteRequestPage from "./pages/Quote/Customer/TotalQuoteRequestPage";
-import ShopQuoteRequestPage from "./pages/Quote/Customer/ShopQuoteRequestPage";
-import QuoteRequestDetailPage from "./pages/Quote/Customer/QuoteRequestDetailPage";
-import GroomerQuote from "./pages/Groomer/GroomerQuote";
-import GroomerQuoteDetailPage from "./pages/Quote/Groomer/GroomerQuoteDetailPage";
-import GroomerQuoteRequestPage from "./pages/Quote/Groomer/GroomerQuoteRequestPage";
-import GroomerReservationMain from "./pages/Reservation/Groomer/GroomerReservationMain";
-import GroomerReservationDetail from "./pages/Reservation/Groomer/GroomerReservationDetail";
-import TestPage from "./pages/TestPage";
-import MypageBoth from "./pages/MypageBoth";
-import ChatClient from "./pages/Chat/ChatClient";
+import PrivateRoute from "./Routes/PrivateRoutes";
 
 function App() {
-  const { isLoggedIn } = useAuthStore();
+  const { isLoggedIn, DefaultRole } = useAuthStore();
 
   return (
     <div className="App">
       <Router>
         <Routes>
           <Route path="/*" element={<PublicRoutes />} />
-          <Route path="/customer/*" element={<CustomerRoutes />} />
-          <Route path="/groomer/*" element={<GroomerRoutes />} />
+
+          <Route
+            path="/customer/*"
+            element={
+              <PrivateRoute isLoggedIn={isLoggedIn} role="customer" userRole={DefaultRole}>
+                <CustomerRoutes />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/groomer/*"
+            element={
+              <PrivateRoute isLoggedIn={isLoggedIn} role="groomer" userRole={DefaultRole}>
+                <GroomerRoutes />
+              </PrivateRoute>
+            }
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
